47ee235ecb1a3c59e94155b44dfaa47516888f6b
philipmogul <philipp1mogul@gmail.com>
2026-01-27 14:30:14 +0300

    Conditions & format() syntax

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+a, b, c = 5, 10, 15
+
+print("Comparing a ({}) and b ({}) and c ({}):".format(a, b, c))
+
+if a > b:
+    print("a ({}) is greater than b ({})".format(a, b))
+elif a < b:
+    print("a ({}) is less than b ({})".format(a, b))
+else:
+    print("a ({}) is equal to b ({})".format(a, b))
