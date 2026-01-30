c6bdcfa442d2b09eaa6dad32e9a6053ea2de30e2
philipmogul <philipp1mogul@gmail.com>
2026-01-30 13:02:03 +0300

    Python raw strings & multiline strings

---
+++ strings.py
@@ -0,0 +1,8 @@
+rawstring = r"This is a raw string with a backslash: \n and a tab: \t"
+print("Raw String Example:")
+
+multiline_string = """\
+This is a multiline string.
+It can span multiple lines.
+Tabs and new lines are preserved."""
+print(multiline_string)
