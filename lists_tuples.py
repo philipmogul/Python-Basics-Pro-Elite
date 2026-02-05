edacb67542de81d59d0ecae837019d936a4756ce
philipmogul <philipp1mogul@gmail.com>
2026-02-05 19:15:50 +0300

    Constructors for lists, tuples & dictionaries: list(), tuple(), dict()

--- lists_tuples.py
+++ lists_tuples.py
@@ -13,4 +13,25 @@ def main():
     print("First 5 characters:", slices[:5])
     print("Characters from index 7 to 12:", slices[7:12])
 
+    # important notes about tuples
+    print("Tuples are created using commas, not parentheses.")
+    tup = 1,
+    print("Single element tuple:", tup)
+    print("Type of single element tuple:", type(tup))
+    print("Lists can be created using the list() constructor:")
+    li = list((1, 2, 3))
+    li2 = list(range(11))
+    print("List created from tuple:", li)
+    print("List created from range:", li2)
+
+    print("Tuples can be created using the tuple() constructor:")
+    tup2 = tuple([1, 2, 3])
+    print("Tuple created from list:", tup2)
+    print("Tuples can be used as keys in dictionaries, while lists cannot.")
+
+    print("Dictionaries can be created using the dict() constructor:")
+    d = dict([(1, "one"), (2, "two"), (3, "three")])
+    print("Dictionary created from list of tuples:", d)
+
+
 if __name__ == "__main__": main()
